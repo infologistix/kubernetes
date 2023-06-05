a5174b8e2077911c29a72c55e13977bc5d61cf02
MariePad <marie.padberg@infologistix.de>
2023-06-05 17:46:46 +0200

    Deploy website - based on ded955dc16e0578346e58485dca65462b7bf9ed3

--- assets/js/18bf20ad.8748fd6b.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkkubernetes=self.webpackChunkkubernetes||[]).push([[503],{91:e=>{e.exports=JSON.parse('{"permalink":"/kubernetes/blog/tags/python-3","page":1,"postsPerPage":2,"totalPages":1,"totalCount":2,"blogDescription":"Blog","blogTitle":"Cloud Engineering Blog"}')}}]);
---
+++ assets/js/78029720.89dae23d.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkkubernetes=self.webpackChunkkubernetes||[]).push([[16],{7480:e=>{e.exports=JSON.parse('{"permalink":"/kubernetes/blog/tags/azure","page":1,"postsPerPage":2,"totalPages":1,"totalCount":2,"blogDescription":"Blog","blogTitle":"KubeSpectra Blog"}')}}]);
